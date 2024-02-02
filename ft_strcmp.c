d0a72e1e55dbb851b778aaa6e748a552c5deba7c
dansylvain <dansylvain84@gmail.comgit config --global user.email dansylvain84@gmail.com>
2024-02-02 17:12:14 +0100

    added func files

---
+++ ft_strcmp.c
@@ -0,0 +1,4 @@
+int ft_strcmp(char *str1, char *str2)
+{
+	
+}
--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+#include "header.h"
+
+
 int main(int argc, char **argv)
 {
 	char str[] = "string to compare";
 
 	printf("GIT WORKSHOP!\n");
 	printf("ft_strlen: %i\n", ft_strlen(argv[1]));
-	printf("ft_putchar: %c\n", ft_putchar(argv[1]));
-    printf("ft_strcmp: %d\n", ft_strcmp(argv[1]));
-    ft_split(argv[1]);
+	printf("ft_putchar: ");
+	ft_putchar(argv[1][0]);
+	printf("\n");
+    printf("ft_strcmp: %d\n", ft_strcmp(argv[1], str));
+    ft_split(argv[1], ' ');
     printf("ft_split:\n");
 	return (0);
 }
